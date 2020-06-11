23d566f3e240ae5fa9b00b989034d1db6789e1d0
jahangirpial <jahangiralompial@gmail.com>
2020-06-11 23:44:20 +0600

    Update Bus Company

--- buses/urls.py
+++ buses/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from BusTicket import views
+from buses import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index),
-    path('buses/', include('buses.urls')),
+    path('update/<int:pk>', views.update),
 ]
--- buses/views.py
+++ buses/views.py
@@ -4,7 +4,17 @@ from django.shortcuts import render
 from buses.models import BusCompany
 
 
-def bus_companies(request: HttpRequest):
+def index(request: HttpRequest):
     buses = BusCompany.objects.all().order_by('name')
     return render(request, 'buses/index.html', {'buses': buses})
 
+
+def update(request: HttpRequest, pk):
+    bus = BusCompany.objects.get(id=pk)
+    if request.method == 'POST':
+        bus.name = request.POST.get('name')
+        bus.head_office = request.POST.get('head_office')
+        bus.staff_count = request.POST.get('staff_count')
+        bus.save()
+
+    return render(request, 'buses/update.html', {'bus': bus})
