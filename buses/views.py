7f7a7c44937d135642d084a8fbffefadde01b55c
jahangirpial <jahangiralompial@gmail.com>
2020-06-16 00:06:34 +0600

    User Group Permission

--- buses/views.py
+++ buses/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from buses.models import BusCompany
@@ -9,26 +10,35 @@ class CompanyList(ListView):
     context_object_name = 'buses'
 
 
-class CompanyCreate(CreateView):
+class CompanyCreate(UserPassesTestMixin, CreateView):
     model = BusCompany
     template_name = 'buses/create.html'
     fields = ('name', 'head_office', 'staff_count')
     success_url = '/buses'
 
+    def test_func(self):
+        return self.request.user.has_perm('buses.add_buscompany')
 
-class CompanyUpdate(UpdateView):
+
+class CompanyUpdate(UserPassesTestMixin, UpdateView):
     model = BusCompany
     template_name = 'buses/update.html'
     fields = ('name', 'head_office', 'staff_count')
     success_url = '/buses'
     context_object_name = 'bus'
 
+    def test_func(self):
+        return self.request.user.has_perm('buses.change_buscompany')
 
-class CompanyDelete(DeleteView):
+
+class CompanyDelete(UserPassesTestMixin, DeleteView):
     model = BusCompany
     template_name = 'buses/delete.html'
     success_url = '/buses'
     context_object_name = 'bus'
+
+    def test_func(self):
+        return self.request.user.has_perm('buses.delete_buscompany')
 
 
 
